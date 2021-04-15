f1c3d9e566ba8d45003fc4fca88346b8763e0232
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2021-04-15 15:56:14 -0500

    Add files via upload

--- random.js
+++ random.js
@@ -3,15 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
 	var random = 0;
 	var fact = "This fact ~should not show up.!";
 	var container = document.querySelector(".centered");
-	document.addEventListener("click", event => {
-		fetch(request).then(response => response.json()).then(data => {
-			random = Math.floor(Math.random() * data.facts.length);
-			fact = "Fact number *" + String(random + 1) + "!<br>" + data.facts[random];
-			container.innerHTML = fact
-				.replaceAll("!", "</span>")
-				.replaceAll("*", "<span class='big blue'>")
-				.replaceAll("~", "<span class='blue'>")
-				.replaceAll("`", "<span class='italic blue'>");
-		});
+	document.addEventListener("keydown", event => {
+		if(event.keyCode === 32) {
+			fetch(request).then(response => response.json()).then(data => {
+				random = Math.floor(Math.random() * data.facts.length);
+				fact = "Fact number *" + String(random + 1) + "!<br>" + data.facts[random];
+				container.innerHTML = fact
+					.replaceAll("!", "</span>")
+					.replaceAll("*", "<span class='big blue'>")
+					.replaceAll("~", "<span class='blue'>")
+					.replaceAll("`", "<span class='italic blue'>");
+			});
+		}
 	});
 });
